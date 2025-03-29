ba7e448349ff674e16a51a2299a347e12e8997c6
Kadir Köroğlu <kadirkoroglu10@gmail.com>
2025-03-29 16:10:36 +0300

    Devam Çalışmaları

---
+++ C# Video Dersleri Asıl/16. Operatorler Projesi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.Operatorler_Projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 23;
+            sayi++;
+
+            int sayi2 = 52;
+            sayi2 += 8;
+
+            if (sayi == 24)
+            {
+                Console.WriteLine("sayi artırılmıs");
+            }
+            else 
+            {
+                Console.WriteLine(sayi);
+            }
+            
+
+            Console.WriteLine(sayi2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C# Video Dersleri Asıl/16. Operatorler Projesi/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("16. Operatorler Projesi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("16. Operatorler Projesi")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("69ee55b3-124f-46de-ab45-da05ffb7e2ce")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
